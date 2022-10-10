a87d7c664d22224acd0b039bbe8c0b77a6dc6e84
EstebanZG999 <zam22119@uvg.edu.gt>
2022-10-09 20:58:06 -0600

    Getter y setters para ServicioEmergencia

--- Contacto.java
+++ Contacto.java
@@ -2,26 +2,15 @@ public class Contacto {
     private String nombre;
     private long numero;
 
-<<<<<<< Updated upstream
-    public Contacto() {
-    }
 
     public Contacto(String nombre, long numero) {
-=======
-    public Contacto(String nombre, long numero){
->>>>>>> Stashed changes
         this.nombre = nombre;
         this.numero = numero;
     }
 
-<<<<<<< Updated upstream
 
     public String getNombre() {
         return this.nombre;
-=======
-    public String getNombre() {
-        return nombre;
->>>>>>> Stashed changes
     }
 
     public void setNombre(String nombre) {
@@ -29,19 +18,12 @@ public class Contacto {
     }
 
     public long getNumero() {
-<<<<<<< Updated upstream
         return this.numero;
-=======
-        return numero;
->>>>>>> Stashed changes
     }
 
     public void setNumero(long numero) {
         this.numero = numero;
     }
-<<<<<<< Updated upstream
 
 
-=======
->>>>>>> Stashed changes
 }
