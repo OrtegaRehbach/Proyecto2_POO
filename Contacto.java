c71ae99278e061ea2974d84021c134241596b4dd
EstebanZG999 <zam22119@uvg.edu.gt>
2022-10-09 20:56:07 -0600

    Getter y setters para contacto

--- Contacto.java
+++ Contacto.java
@@ -2,17 +2,26 @@ public class Contacto {
     private String nombre;
     private long numero;
 
+<<<<<<< Updated upstream
     public Contacto() {
     }
 
     public Contacto(String nombre, long numero) {
+=======
+    public Contacto(String nombre, long numero){
+>>>>>>> Stashed changes
         this.nombre = nombre;
         this.numero = numero;
     }
 
+<<<<<<< Updated upstream
 
     public String getNombre() {
         return this.nombre;
+=======
+    public String getNombre() {
+        return nombre;
+>>>>>>> Stashed changes
     }
 
     public void setNombre(String nombre) {
@@ -20,12 +29,19 @@ public class Contacto {
     }
 
     public long getNumero() {
+<<<<<<< Updated upstream
         return this.numero;
+=======
+        return numero;
+>>>>>>> Stashed changes
     }
 
     public void setNumero(long numero) {
         this.numero = numero;
     }
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
 }
